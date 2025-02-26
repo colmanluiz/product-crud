11fabacce43a383ae36e218d04d5566d7ba9cc21
Luiz Colman <colmanluizz@gmail.com>
2025-02-26 12:46:59 -0300

    configure Mongoose connection and add global validation pipe

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import 'dotenv/config';
+import { ProductsModule } from './products/products.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot(`${process.env.MONGO_URI}`),
+    ProductsModule,
+    CategoriesModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
